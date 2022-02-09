cf8a01ab2c2f1a961bed2bb7721e442e8c738c74
Erick de Oliveira <eobx@hotmail.com>
2022-02-09 01:59:14 -0300

    ajustes

--- my-app/src/componentes/paginas/SobreNos/index.js
+++ my-app/src/componentes/paginas/SobreNos/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import "./index.css";
-const Contato = ()=>{
-    return(<>
-        <div className="pagdescription" >
+const Contato = () => {
+    return (<>
+
         <h1>SOBRE NÓS</h1>
+
+        <div className="sobrenos-section">
+            <div className="sobrenos-01">
+                Bla bla bla
+            </div>
+            <div className="sobrenos-02">
+                <form>
+                    <input name="name" type="text" class="feedback-input" placeholder="Nome" />
+                    <input name="email" type="text" class="feedback-input" placeholder="E-mail" />
+                    <textarea name="text" class="feedback-input" placeholder="Comentar"></textarea>
+                    <input type="enviar" value="ENVIAR" />
+                </form>
+            </div>
         </div>
-        <div className="sobrenos">
-            Bla bla bla
-        </div></>
+
+
+
+
+    </>
+
     );
 }
 
